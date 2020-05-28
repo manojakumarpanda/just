43fbe836b42a7ab1368419d82aea5b7c8ff73ad3
Manoj Panda <kumarpandamanoja@gmail.com>
2020-05-28 16:34:10 +0530

    some mess

--- class.py
+++ class.py
@@ -1,4 +1,36 @@
+#This program is for the Operator overriding
 
 
+
+#This is for the add method
+
+class Operator:
+
+    def __init__(self,a,b,c):
+        self.a=a
+        self.b=b
+        self.c=c
+        self.result=None
+
+    def __add__(self,oth):
+        print('The number of time call')
+        self.res1=self.a+oth.a
+        self.res2=self.b+oth.b
+        self.res3=self.c+oth.c
+        return Operator(self.res1,self.res2,self.res3)
+
+    def __str__(self):
+        return 'The result of {} and {} and {} after perform operation is:{}'.format(self.a,self.b,self.c,self.result)
+
+    
+
+#Creating the objects of the class
+op=Operator(10,11,21)
+op1=Operator(32,12,12)
+op3=Operator(342,13,10)
+x=op+op1+op3
+print(x.a)
+print(x.b)
+print(x.c)
         
             
